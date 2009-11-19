8e784870e579412f08c51abc605ff3dcb4e65291
Dan Leinir Turthra Jensen <admin@leinir.dk>
2009-11-19 21:10:46 +0100

    small test app + test gdl

---
+++ testapp/btcomponents-testapp.cpp
@@ -0,0 +1,35 @@
+#include "btcomponents-testapp.h"
+
+#include <QtCore/QCoreApplication>
+#include <QtCore/QDebug>
+#include <QtCore/QUrl>
+
+#include <gluon/gluonobjectfactory.h>
+#include <gluon/gameproject.h>
+#include <gluon/game.h>
+#include <gluon/gdlhandler.h>
+
+btcomponentstestapp::btcomponentstestapp()
+{
+    Gluon::GluonObjectFactory::instance()->loadPlugins();
+}
+
+btcomponentstestapp::~btcomponentstestapp()
+{}
+
+void
+btcomponentstestapp::run(QUrl gdlFile)
+{
+    Gluon::GameProject *gameProject = new Gluon::GameProject(QCoreApplication::instance());
+    gameProject->loadFromFile(gdlFile);
+    
+    Gluon::Game::instance()->setParent(QCoreApplication::instance());
+    Gluon::Game::instance()->setGameProject(gameProject);
+    
+    // Run the game at severely reduced speed (one frame per second) so we can
+    // actually see what's happening
+    //Gluon::Game::instance()->runGameFixedTimestep(1);
+    qDebug() << "The gameProject turned into GDL:" << gameProject->toGDL();
+}
+
+#include "btcomponents-testapp.moc"
